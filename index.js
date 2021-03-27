358197924292922d93f5b9aaffe5daea7fa209a2
Lucas <ljmitchell200@gmail.com>
2021-03-27 23:43:29 +1100

    Add first web3 function to retrieve current gas price.

---
+++ index.js
@@ -0,0 +1,8 @@
+async function main() {
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
+    const web3 = createAlchemyWeb3("https://eth-mainnet.alchemyapi.io/v2/meMVEhLc1VBSRRDRA1eT31eZWZ1aou-r")
+    const gasPrice = await web3.eth.getGasPrice()
+    console.log("The current gas price is " + gasPrice)
+  }
+  main()
+  
