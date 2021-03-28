a141f636143d2412d772732a8b882b0a992e6bee
Lucas <ljmitchell200@gmail.com>
2021-03-28 17:14:31 +1100

    Add logic to interact with contract to update state variable

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,9 @@
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
    solidity: "0.7.3",
@@ -17,4 +18,9 @@ module.exports = {
          accounts: [`0x${PRIVATE_KEY}`]
       }
    },
+   etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: "C75YVZC1FHBS5Q9AAK8UW85M34EKDKNDS1"
+   }
 }
